da85a9c6b70ea394baf665a176f1519dd06fb589
Bruce Decker <bruce.d008@gmail.com>
2018-04-15 21:06:57 -0700

    Update Go API

--- backend/user/main.go
+++ backend/user/main.go
@@ -9,6 +9,17 @@ import (
 	"github.com/go-redis/redis"
 )
 
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+type Author struct {
+	Firstname string `json: "firstname"`
+	Lastname  string `json: "lastname"`
+}
+
 func main() {
 
 	client := redis.NewClient(&redis.Options{
