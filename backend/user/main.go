8375c67143eebca7bdd6cacbb6355987b080c886
Bruce Decker <bruce.d008@gmail.com>
2018-04-15 22:25:34 -0700

    Update GO API

--- backend/user/main.go
+++ backend/user/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	//"encoding/json"
 	"encoding/json"
-	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
-	//"log"
+	"strconv"
 
-	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 )
 
+//Book Struct (Model)
 type Book struct {
 	ID     string  `json:"id"`
 	Isbn   string  `json:"isbn"`
@@ -22,6 +22,7 @@ type Author struct {
 	Lastname  string `json: "lastname"`
 }
 
+//Init books var as as slice Book struct
 var books []Book
 
 //Get all books
@@ -44,26 +45,64 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&Book{})
 }
 
+//Create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000)) //Mock ID - not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(books)
+}
+
+//updateBook
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = strconv.Itoa(rand.Intn(1000000)) //Mock ID - not safe
+			books = append(books, book)
+			json.NewEncoder(w).Encode(books)
+			return
+
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+
+}
+
 func main() {
+	//Init Router
+	r := mux.NewRouter()
 
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+	//Mock data - @todo- implement DB
+	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Smith"}})
+	books = append(books, Book{ID: "2", Isbn: "847564", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Jobs"}})
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
+	//Ruote handlers/ Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
-	employee1 := &Employee{Firstname: "John", Lastname: "Smith", Gender: "Male", Age: "27", Salary: 60000}
-	data, err := json.Marshal(&employee1)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(data))
-	err = client.Set("key", data, 0).Err()
-	if err != nil {
-		panic(err)
-	}
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
