c022ba0e249788b44996ef661cd023529085a83c
Aaditya Deowanshi <aadityadeowanshi@hotmail.com>
2018-04-21 20:06:30 -0700

    Fixed cors issue

--- backend/payment/src/goapi/server.go
+++ backend/payment/src/goapi/server.go
@@ -223,7 +223,13 @@ func makePaymentHandler(formatter *render.Render) http.HandlerFunc {
 
 func getPaymentHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods","POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Content-Type", "application/json")
+		if req.Method == "OPTIONS"{
+			return
+		}
 		c := NewClient(serverElb)
 		params := mux.Vars(req)
 		var uid string = params["id"]
