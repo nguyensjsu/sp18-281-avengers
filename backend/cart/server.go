f7ad069f273692005e0c456650272e86ef209869
Sahil Sharma <sahil.sharma@sjsu.edu>
2018-04-28 17:32:15 -0700

    resolving cors

--- backend/cart/server.go
+++ backend/cart/server.go
@@ -252,13 +252,18 @@ func init() {
 }
 
 func initRoutes(mx *mux.Router, formatter *render.Render) {
-	mx.HandleFunc("/", pingHandler(formatter)).Methods("GET")
+mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
 	mx.HandleFunc("/order", createOrderHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/order", createOrderHandler(formatter)).Methods("OPTIONS")
 	mx.HandleFunc("/order/{id}", updateCartHandler(formatter)).Methods("PUT")
+	mx.HandleFunc("/order/{id}", updateCartHandler(formatter)).Methods("OPTIONS")
 	mx.HandleFunc("/view/{id}", getOrderHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/view/{id}", getOrderHandler(formatter)).Methods("OPTIONS")
 	mx.HandleFunc("/history/{id}", viewCartHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/history/{id}", viewCartHandler(formatter)).Methods("OPTIONS")
 	mx.HandleFunc("/clearCart/{id}", clearCartHandler(formatter)).Methods("DELETE")
 	mx.HandleFunc("/process", processCartHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/process", processCartHandler(formatter)).Methods("OPTIONS")
 }
 
 func failOnError(err error, msg string) {
@@ -272,6 +277,13 @@ func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
 	fmt.Fprintf(w, "{message: %q}", message)
+}
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(*w).Header().Set("Content-Type", "application/json")
 }
 
 unc pingHandler(formatter *render.Render) http.HandlerFunc {
