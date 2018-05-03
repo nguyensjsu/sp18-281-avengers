1aeab26735918e6cd1178b2b323c5d9af3921606
Sahil Sharma <sahil.sharma@sjsu.edu>
2018-05-02 17:39:27 -0700

    testFront

--- backend/cart/testFront/cart.js
+++ backend/cart/testFront/cart.js
@@ -70,3 +70,67 @@ function doneOrder() {
                 console.log(error);
             });
 }
+
+/ Populate cart for current order and current user.
+function populateCart() {
+    console.log("[DEBUG CART]: Populate cart called.")
+
+    var orderId = localStorage.getItem("orderid")
+
+    console.log("[DEBUG CART]: Order ID is: ", orderId);
+
+    axios.get('http://localhost:3000/view/' + orderId)
+        .then(function (response) {
+            // console.log(response.data.id);
+            // console.log("[CART DEBUG] Fetched from database: ", response.data)
+            // alert(response.data);
+
+            // console.log("[DEBUG]: ", response.data.items.length)
+
+            var result=[];
+            for(var i=0;i<response.data.items.length;i++){
+                console.log("[DEBUG LOOP]: Inside for loop.")
+                result.push(response.data.items[i]);
+                // console.log("[DEBUG DATA STORE]: ", result[i])
+            }
+            console.log("[DEBUG]: Length of items: ", result.length);
+
+            // Create table structure
+            var table = '<style>' +
+                'table {' +
+                    'font-family: arial, sans-serif;' +
+                    'border-collapse: collapse;' +
+                    'width: 70%;text-align:center;' +
+                    'border-radius:2px}' +
+                'td,th {' +
+                    'border: 1px solid #dddddd;' +
+                    'text-align: center;' +
+                    'padding: 8px;}' +
+                'th {' +
+                    'background-color:#e8c592;}' +
+                'td {' +
+                    'background-color:#ffffcc;}' +
+                '</style>' +
+                '<table align="center" cellpadding="2" style="text-align:center;font-family: arial,sans-serif;border-collapse: collapse>"';
+
+            table +='<tr>' +
+                    '<th>ITEM NAME</th><th>COUNT</th><th>RATE</th><th>AMOUNT</th>' +
+                '</tr>';
+
+
+            for(var i=0;i<result.length;i++){
+                /* console.log("in loop");*/
+                console.log("[DEBUG]: Amount: ", result[i].amount);
+                table += '<em></em><tr><td>' +
+                    result[i].name +
+                    '</td><td>' + result[i].count + '</td><td>' + result[i].rate + '</td><td>' + result[i].amount + '</td>' +
+                    '</tr></em>';
+                //console.log(response.data[i]);
+            }
+            table += '</table>';
+            document.getElementById('itemlist').innerHTML = table;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
